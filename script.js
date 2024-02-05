885947d4a9fa18ac51038a5f9088ec9d579dc353
ana <156020885+makia014@users.noreply.github.com>
2024-02-05 22:32:50 +0200

    Add function playRound

--- script.js
+++ script.js
@@ -2,10 +2,32 @@ function getComputerChoice() {
   let computerChoice = Math.floor(Math.random() * 3 + 1);
   switch (computerChoice) {
     case 1:
-      return "Rock";
+      return "rock";
     case 2:
-      return "Paper";
+      return "paper";
     case 3:
-      return "Scissors";
+      return "scissors";
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return `This round is a tie! You both drew ${computerSelection}.`;
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+  ) {
+    return `You win this round as ${playerSelection} beats ${computerSelection}.`;
+  } else if (
+    (computerSelection === "rock" && playerSelection === "scissors") ||
+    (computerSelection === "scissors" && playerSelection === "paper") ||
+    (computerSelection === "paper" && playerSelection === "rock")
+  ) {
+    return `Computer wins this round as ${computerSelection} beats ${playerSelection}.`;
+  }
+}
+
+computerSelection = getComputerChoice();
+playerSelection = "paper";
+console.log(playRound(playerSelection, computerSelection));
