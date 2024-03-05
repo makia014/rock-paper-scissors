ed6ea717cbd047b1f782eda5f61415b619a18513
ana <156020885+makia014@users.noreply.github.com>
2024-03-05 22:11:16 +0200

    Add UI

--- script.js
+++ script.js
@@ -10,56 +10,94 @@ function getComputerChoice() {
   }
 }
 
-let roundWinner;
+let playerPoints;
+let computerPoints;
+const field = document.querySelector(".field");
+const stats = document.createElement("p");
+field.prepend(stats);
+const start = document.querySelector(".start");
+const divs = document.querySelectorAll("div");
+const btns = document.querySelector(".btns");
+const choice = document.querySelectorAll(".choice");
+const choices = document.querySelector(".choices");
+const playerScore = document.querySelector(".you");
+const computerScore = document.querySelector(".computer");
+const winner = document.createElement("h3");
+document.body.insertBefore(winner, start);
+
+const addToScreen = (el, string) => {
+  el.textContent = string;
+};
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    roundWinner = "tie";
-    return `This round is a tie! You both drew ${computerSelection}.`;
+    addToScreen(
+      stats,
+      `This round is a tie! You both drew ${playerSelection}.`
+    );
   } else if (
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "scissors" && computerSelection === "paper") ||
     (playerSelection === "paper" && computerSelection === "rock")
   ) {
-    roundWinner = "player";
-    return `You win this round as ${playerSelection} beats ${computerSelection}.`;
+    addToScreen(
+      stats,
+      `Computer chose ${computerSelection}, but you chose ${playerSelection}. You win this round!`
+    );
+    playerPoints++;
   } else if (
     (computerSelection === "rock" && playerSelection === "scissors") ||
     (computerSelection === "scissors" && playerSelection === "paper") ||
     (computerSelection === "paper" && playerSelection === "rock")
   ) {
-    roundWinner = "computer";
-    return `Computer wins this round as ${computerSelection} beats ${playerSelection}.`;
+    addToScreen(
+      stats,
+      `Computer wins this round as it chose ${computerSelection}, which beats your ${playerSelection}.`
+    );
+    computerPoints++;
   }
 }
 
-function playGame() {
-  let playerPoints = 0;
-  let computerPoints = 0;
-  for (let i = 0; i < 5; i++) {
-    let playerInput = prompt("Enter your choice: ");
-    let playersChoice = playerInput.toLowerCase();
-    let computersChoice = getComputerChoice();
-    console.log(playRound(playersChoice, computersChoice));
-    if (roundWinner === "player") {
-      playerPoints++;
-      console.log(`You get 1 point.`);
-    } else if (roundWinner === "computer") {
-      computerPoints++;
-      console.log(`Computer gets 1 point.`);
-    } else {
-      console.log("Both you and computer get no points for the round.");
+const getWinner = () => {
+  if (computerPoints == 5 || playerPoints == 5) {
+    computerPoints < playerPoints
+      ? addToScreen(winner, "The winner is you!")
+      : addToScreen(winner, "The winner is computer!");
+    winner.classList.add("finish");
+    toggleField();
+  }
+};
+
+function toggleField() {
+  divs.forEach((div) => {
+    div.classList.toggle("hidden");
+  });
+  choice.forEach((el) => {
+    el.classList.toggle("hidden");
+  });
+  addToScreen(stats, "");
+  computerPoints = 0;
+  playerPoints = 0;
+  addToScreen(computerScore, computerPoints);
+  addToScreen(playerScore, playerPoints);
+}
+
+start.addEventListener("click", (e) => {
+  e.stopImmediatePropagation();
+  toggleField();
+  roundWinner = "";
+  addToScreen(winner, roundWinner);
+  winner.classList.remove("finish");
+
+  choices.addEventListener("click", (e) => {
+    e.stopImmediatePropagation();
+    if (e.target.classList.contains("choice")) {
+      let cC = getComputerChoice();
+      let pC = e.target.title;
+      playRound(pC, cC);
+      addToScreen(computerScore, computerPoints);
+      addToScreen(playerScore, playerPoints);
     }
-  }
-  if (playerPoints >= computerPoints) {
-    console.log(
-      `You: ${playerPoints}. Computer: ${computerPoints}. You win this game!!!`
-    );
-  } else {
-    console.log(
-      `You: ${playerPoints}. Computer: ${computerPoints}. Computer wins this game!!!`
-    );
-  }
-}
-
-playGame();
+    getWinner();
+  });
+});
